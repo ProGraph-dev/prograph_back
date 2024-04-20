a125ee08639fdd34f22c9b6abbc4c2eaa14ecd73
Garik <qgagk99@gmail.com>
2024-04-20 13:18:03 +0400

    added auth and user modules

---
+++ src/modules/user/entity/user.entity.ts
@@ -0,0 +1,19 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar', nullable: false })
+  firstName: string;
+
+  @Column({ type: 'varchar', nullable: false })
+  lastName: string;
+
+  @Column({ type: 'varchar' })
+  email: string;
+
+  @Column({ type: 'varchar' })
+  password: string;
+}
