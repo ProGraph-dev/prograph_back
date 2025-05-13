884b2ec589d25e5e548aeb001b5219effb4d2ffa
Garik <qgagk99@gmail.com>
2025-05-13 14:07:04 +0400

    fixed chat mistake

--- src/modules/chat/chat.service.ts
+++ src/modules/chat/chat.service.ts
@@ -131,39 +131,43 @@ export class ChatService {
     try {
       const list = await this._chatRepo.query(`
           SELECT
-            chat."id",
-            json_agg(
-              DISTINCT jsonb_build_object(
-                'id', u."id",
-                'firstName', u."firstName",
-                'lastName', u."lastName",
-                'role', u."userRole"
+          chat."id",
+          jsonb_build_object(
+            'id', project."id",
+            'title', project.title
+          ) AS project,
+          json_agg(
+            DISTINCT jsonb_build_object(
+              'id', u."id",
+              'firstName', u."firstName",
+              'lastName', u."lastName",
+              'role', u."userRole"
+            )
+          ) AS users,
+          CASE
+            WHEN m.id IS NOT NULL THEN json_build_object(
+              'id', m."id",
+              'text', m."text",
+              'url', m.url,
+              'type', m."type",
+              'createdAt', m."createdAt",
+              'sender', jsonb_build_object(
+                'id', sender.id,
+                'firstName', sender."firstName",
+                'lastName', sender."lastName",
+                'avatar', sender.avatar
               )
-            ) AS users,
-            CASE
-              WHEN m.id IS NOT NULL THEN json_build_object(
-                'id', m."id",
-                'text', m."text",
-                'url', m.url,
-                'type', m."type",
-                'createdAt', m."createdAt",
-                'sender', jsonb_build_object(
-                  'id', sender.id,
-                  'firstName', sender."firstName",
-                  'lastName', sender."lastName",
-                  'avatar', sender.avatar
-                )
-              )
-              ELSE NULL
-            END AS message,
-            (
-              SELECT COUNT(message."id")
-              FROM message
-              LEFT JOIN user_readed_mesages as mr
-                ON mr."messageId" = message."id" AND mr."userId" = ${user_id}
-              WHERE message."chatId" = chat."id"
-                AND mr."messageId" IS NULL
-            ) AS unreaded_count
+            )
+            ELSE NULL
+          END AS message,
+          (
+            SELECT COUNT(message."id")
+            FROM message
+            LEFT JOIN user_readed_mesages AS mr
+              ON mr."messageId" = message."id" AND mr."userId" = ${user_id}
+            WHERE message."chatId" = chat."id"
+              AND mr."messageId" IS NULL
+          ) AS unreaded_count
           FROM chat
           INNER JOIN chat_user cu1 ON cu1."chatId" = chat."id"
           INNER JOIN chat_user cu2 ON cu2."chatId" = chat."id"
@@ -175,9 +179,11 @@ export class ChatService {
             ORDER BY message."createdAt" DESC
             LIMIT 1
           ) AS m ON true
-          LEFT JOIN "user" as sender ON sender."id" = m."senderId"
+          LEFT JOIN "project" ON "project".id = chat."projectId"
+          LEFT JOIN "user" AS sender ON sender."id" = m."senderId"
           WHERE cu1."userId" = ${user_id}
           GROUP BY chat."id",
+                   project.id, project.title,
                    m.id, m.text, m.url, m.type, m."createdAt",
                    sender.id, sender."firstName", sender."lastName", sender.avatar
           ORDER BY m."createdAt" DESC NULLS LAST
